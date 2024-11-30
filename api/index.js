739d12de713cea09d4fd961443ff405a7f954d4e
Thiagouh <thiagolorhandasilva12@gmail.com>
2024-11-30 12:23:17 -0300

    feat: add listener

--- api/index.js
+++ api/index.js
@@ -25,4 +25,8 @@ app.get("/api/whoami", (req, res) => {
   });
 });
 
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
 module.exports = app;
